a5d5291d4e8ae3c433bdd84ab8463a1c49a2748e
Erin M <erinfranmc@gmail.com>
2019-10-18 17:13:00 +0100

    added first images to project page using gatsby-image

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -3,14 +3,75 @@ import { Link } from "gatsby"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
-
-const ProjectsPage = () => (
+import Img from "gatsby-image"
+const ProjectsPage = props => (
   <Layout>
     <SEO title="Projects" />
     <h1>My Projects</h1>
     <p>Here are some of my projects</p>
+    <Img fluid={props.data.imageOne.childImageSharp.fluid} />
+    <Img fluid={props.data.imageTwo.childImageSharp.fluid} />
+    <Img fluid={props.data.imageThree.childImageSharp.fluid} />
+    <Img fluid={props.data.imageFour.childImageSharp.fluid} />
+    <Img fluid={props.data.imageFive.childImageSharp.fluid} />
+    <Img fluid={props.data.imageSix.childImageSharp.fluid} />
+    <Img fluid={props.data.imageSeven.childImageSharp.fluid} />
     <Link to="/">Go back to the homepage</Link>
   </Layout>
 )
 
 export default ProjectsPage
+
+export const pageQuery = graphql`
+  query {
+    imageOne: file(relativePath: { eq: "ids-1.png" }) {
+      childImageSharp {
+        fluid(maxWidth: 1000) {
+          ...GatsbyImageSharpFluid
+        }
+      }
+    }
+    imageTwo: file(relativePath: { eq: "ids-2.png" }) {
+      childImageSharp {
+        fluid(maxWidth: 1000) {
+          ...GatsbyImageSharpFluid
+        }
+      }
+    }
+    imageThree: file(relativePath: { eq: "ids-3.png" }) {
+      childImageSharp {
+        fluid(maxWidth: 1000) {
+          ...GatsbyImageSharpFluid
+        }
+      }
+    }
+    imageFour: file(relativePath: { eq: "ids-4.png" }) {
+      childImageSharp {
+        fluid(maxWidth: 1000) {
+          ...GatsbyImageSharpFluid
+        }
+      }
+    }
+    imageFive: file(relativePath: { eq: "ids-5.png" }) {
+      childImageSharp {
+        fluid(maxWidth: 1000) {
+          ...GatsbyImageSharpFluid
+        }
+      }
+    }
+    imageSix: file(relativePath: { eq: "weather-1.png" }) {
+      childImageSharp {
+        fluid(maxWidth: 1000) {
+          ...GatsbyImageSharpFluid
+        }
+      }
+    }
+    imageSeven: file(relativePath: { eq: "weather-2.png" }) {
+      childImageSharp {
+        fluid(maxWidth: 1000) {
+          ...GatsbyImageSharpFluid
+        }
+      }
+    }
+  }
+`
