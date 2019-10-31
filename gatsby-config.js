fb99ba375a90435e7ea3cbed05d5e189a6437cd9
Erin M <erinfranmc@gmail.com>
2019-10-31 12:51:50 +0000

    added favicon, manifest and service worker

--- gatsby-config.js
+++ gatsby-config.js
@@ -30,15 +30,18 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `Erin McGrath: Front-end Developer`,
+        short_name: `Erin McGrath`,
         start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
+        background_color: `#701dc9`,
+        theme_color: `#701dc9`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/female-technologist-favicon.png`, // This path is relative to the root of the site.
       },
     },
+    // this (optional) plugin enables Progressive Web App + Offline functionality
+    // To learn more, visit: https://gatsby.dev/offline
+    `gatsby-plugin-offline`,
     {
       resolve: `gatsby-plugin-google-fonts`,
       options: {
@@ -46,8 +49,5 @@ module.exports = {
         display: "swap",
       },
     },
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
   ],
 }
