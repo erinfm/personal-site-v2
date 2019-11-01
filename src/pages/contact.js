9b766f1f3f2afe388715504e2082feda71e71715
Erin M <erinfranmc@gmail.com>
2019-11-01 17:29:30 +0000

    experimenting with redirects, and removed unused imports from components

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,6 +1,5 @@
 import React from "react"
 import styled from "styled-components"
-import { Link } from "gatsby"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
--- src/pages/index.js
+++ src/pages/index.js
@@ -3,7 +3,6 @@ import { Link } from "gatsby"
 import styled from "styled-components"
 
 import Layout from "../components/layout"
-import Image from "../components/image"
 import SEO from "../components/seo"
 
 const StyledP = styled.p`
