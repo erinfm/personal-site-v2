67c0ccf171549e80a3d3c10500167b79424d563c
Erin M <erinfranmc@gmail.com>
2019-11-02 15:30:50 +0000

    changed styling of blog post list

--- src/components/post-link.js
+++ src/components/post-link.js
@@ -5,13 +5,20 @@ import styled from "styled-components"
 const StyledLink = styled(props => <Link {...props} />)`
   font-weight: 600;
   color: var(--textLink);
-  margin-bottom: 1rem;
-  display: inline-block;
+  margin-bottom: 1.5rem;
+  display: block;
+`
+
+const Date = styled.span`
+  display: block;
+  font-size: 1rem;
+  font-weight: 400;
+  margin-top: 0.25rem;
 `
 
 const PostLink = ({ post }) => (
   <StyledLink to={post.frontmatter.path}>
-    {post.frontmatter.title} ({post.frontmatter.date})
+    {post.frontmatter.title} <Date>{post.frontmatter.date}</Date>
   </StyledLink>
 )
 
